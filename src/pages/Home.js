3e321380efa9c47565f1d417bd420bfe65ac9e58
Mohamed Mohamed <63976154+Mohamed-N-Mohamed@users.noreply.github.com>
2022-04-14 19:36:14 +0100

    changes tailwind config

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='container'>
+      <div className="advice-card p-2 mt-2 w-1/2 h-8 bg-Grayish-Blue">
+
+
+      </div>
+
+
+    </div>
    
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,23 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+
+      container: {
+        padding:'1rem',
+        center: true
+
+      },
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px",
+      },
+      colors: {
+       'Grayish-Blue': 'hsl(217, 19%, 38%)',
+       'Dark-Grayish-Blue': 'hsl(217, 19%, 24%)'
+      }
+    },
   },
   plugins: [],
 }
